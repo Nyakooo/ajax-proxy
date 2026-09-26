ea64dba3c52fa170402b0de4caec299be112674f
Nyakoo <gongjie0422@163.com>
2026-09-26 23:05:33 +0800

    fix(security): upgrade webpack dev server

--- packages/vue-panels/vue.config.js
+++ packages/vue-panels/vue.config.js
@@ -8,9 +8,8 @@ module.exports = {
   devServer: {
     host: "0.0.0.0",
     port: 8082,
-    https: false,
     hot: true, // 开启热模块加载
-    disableHostCheck: true, // 启用 host 访问
+    allowedHosts: "all", // 保持局域网访问
     // proxy: {},
   },
   publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
